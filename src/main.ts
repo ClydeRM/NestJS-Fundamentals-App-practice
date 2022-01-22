e786b8efafd6b5f0364249507d9e67028bd1c2b4
ClydeRM <clyde.official@icloud.com>
2022-01-22 18:04:47 +0800

    feat(App): Using pipe whitelist
    setup globalpipe validator middleware
    1. whitelist : restrict response shape
    2. forbidNonWhitelisted : validate request body data
    3. transform : transform request json to DTO shape
    
    BREAKING CHANGE:

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,13 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // 檢查Request資料完整性
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // 限制response，只回傳DTO規定的內容
+      transform: true, // 自動轉換 request Params, Body的型別成DTO規定的型別
+      forbidNonWhitelisted: true, // 抵擋request，如果request body有DTO規定外的欄位，request被攔截
+    }),
+  );
 
   await app.listen(3000);
 }
