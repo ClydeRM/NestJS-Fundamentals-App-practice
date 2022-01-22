21866d09e8e99b6e52a0160aa81331089169e717
ClydeRM <clyde.official@icloud.com>
2022-01-22 16:35:43 +0800

    feat(DTO): make DTO readable and flexible
    Use mapped-types Partical class to build UpdateCoffeesDto

--- src/coffees/dto/create-coffee.dto.ts
+++ src/coffees/dto/create-coffee.dto.ts
@@ -1,7 +1,12 @@
 // CLI >> nest g class coffees/dto/create-coffee.dto --no-spec
-
+import { IsString } from 'class-validator';
 export class CreateCoffeeDto {
+  @IsString()
   readonly name: string;
+
+  @IsString()
   readonly brand: string;
+
+  @IsString({ each: true }) // 檢查陣列每一個元素
   readonly flavors: string[];
 }
--- src/coffees/dto/update-coffee.dto.ts
+++ src/coffees/dto/update-coffee.dto.ts
@@ -1,8 +1,7 @@
 // CLI >> nest g class coffees/dto/update-coffee.dto --no-spec
 // Update property optional
 
-export class UpdateCoffeeDto {
-  readonly name?: string;
-  readonly brand?: string;
-  readonly flavors?: string[];
-}
+import { PartialType } from '@nestjs/mapped-types'; // 利用原本的DTO去建立一個內容元素是可選擇使用的新DTO
+import { CreateCoffeeDto } from './create-coffee.dto'; // 原本的DTO
+
+export class UpdateCoffeeDto extends PartialType(CreateCoffeeDto) {}
