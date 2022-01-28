f14728d588088659c8ed19602eb05a4bf3470980
ClydeRM <clyde.official@icloud.com>
2022-01-28 17:30:26 +0800

    refactor(CoffeeService): refactor to use real database
    Refactor CRUD logic to use  postgres sql database
    
    BREAKING CHANGE:

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -1,25 +1,24 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
-import { UpdateCoffeeDto } from './dto/update-coffee.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { Coffee } from './entities/coffee.entity';
+
+import { CreateCoffeeDto } from './dto/create-coffee.dto';
+import { UpdateCoffeeDto } from './dto/update-coffee.dto';
 
 @Injectable()
 export class CoffeesService {
-  // mock DB data
-  private coffees: Coffee[] = [
-    {
-      id: 1,
-      name: 'Shipwreck Roast',
-      brand: 'Buddy Brew',
-      flavors: ['chocolate', 'vanilla'],
-    },
-  ];
+  constructor(
+    @InjectRepository(Coffee) // Use Repository register coffee entity to PGSql
+    private readonly coffeeRepository: Repository<Coffee>,
+  ) {}
 
   findAll() {
-    return this.coffees;
+    return this.coffeeRepository.find();
   }
 
-  findOne(id: string) {
-    const coffee = this.coffees.find((item) => item.id === +id);
+  async findOne(id: string) {
+    const coffee = await this.coffeeRepository.findOne(id);
     if (!coffee) {
       // If Not Found
       throw new HttpException(`Coffee #${id} not found`, HttpStatus.NOT_FOUND);
@@ -27,29 +26,26 @@ export class CoffeesService {
     return coffee;
   }
 
-  create(createCoffeeDto: any) {
-    this.coffees.push(createCoffeeDto);
+  create(createCoffeeDto: CreateCoffeeDto) {
+    const coffee = this.coffeeRepository.create(createCoffeeDto); // Create coffee instance
+    return this.coffeeRepository.save(coffee); // Save coffee instance
   }
 
-  update(id: string, updateCoffeeDto: UpdateCoffeeDto) {
-    let updateData: any; // TODO: change type any
-    const updateCoffeeList = this.coffees.map((coffee) => {
-      if (coffee.id === +id) {
-        updateData = {
-          ...coffee,
-          ...updateCoffeeDto,
-        };
-        return updateData;
-      } else return coffee;
+  async update(id: string, updateCoffeeDto: UpdateCoffeeDto) {
+    const coffee = await this.coffeeRepository.preload({
+      // Repository.preload() create new entity base on original entity
+      id: +id,
+      ...updateCoffeeDto,
     });
-    this.coffees = updateCoffeeList;
-    return updateData;
+    if (!coffee) {
+      // coffee not found
+      throw new HttpException(`Coffee #${id} not found`, HttpStatus.NOT_FOUND);
+    }
+    return this.coffeeRepository.save(coffee);
   }
 
-  remove(id: string) {
-    const coffeeIndex = this.coffees.findIndex((item) => item.id === +id);
-    if (coffeeIndex >= 0) {
-      this.coffees.splice(coffeeIndex, 1);
-    }
+  async remove(id: string) {
+    const coffee = await this.coffeeRepository.findOne(id);
+    return this.coffeeRepository.remove(coffee);
   }
 }
