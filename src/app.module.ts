28a0a3fe8f958cea8b27f02cd20b74ebdde11f5d
ClydeRM <clyde.official@icloud.com>
2022-01-28 17:24:47 +0800

    feat(AppModule): Use TypeOrmModule
    Use TypeOrmModule connect to docker-container postgers sql => localhost:5432
    
    BREAKING CHANGE:

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
 
 @Module({
-  imports: [CoffeesModule],
+  imports: [
+    CoffeesModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'pass123',
+      database: 'postgres',
+      autoLoadEntities: true, // If option not found, try reinstall @nestjs/typeorm
+      synchronize: true, // shouldn't be used in production - otherwise you can lose production data #自動產生entity的SQL table
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
