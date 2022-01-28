321a379c33fcd115d58efd6d4875dde6139136d6
ClydeRM <clyde.official@icloud.com>
2022-01-28 17:21:58 +0800

    refactor(entity): Use Repository pattern design
    Refactor Coffee entity
    1. PrimaryColumn id
    2. Column name
    3. Column brand
    4. Column flavors[] (json type, optional)
    
    BREAKING CHANGE:

--- src/coffees/entities/coffee.entity.ts
+++ src/coffees/entities/coffee.entity.ts
@@ -1,7 +1,16 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 // Definition of coffee data entity.
+@Entity() // sql table === 'coffees'
 export class Coffee {
+  @PrimaryGeneratedColumn()
   id: number;
+
+  @Column()
   name: string;
+
+  @Column()
   brand: string;
+
+  @Column('json', { nullable: true }) // data format is json, and field is optional
   flavors: string[];
 }
