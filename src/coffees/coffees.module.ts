fa34a733379fd20c1564b5a73b47612dd91cf550
ClydeRM <clyde.official@icloud.com>
2022-01-30 16:05:14 +0800

    feat(Module): Add new feature Event
    Add Event entity to handle recommendation Transaction

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CoffeesController } from './coffees.controller';
 import { CoffeesService } from './coffees.service';
+
 import { Coffee } from './entities/coffee.entity';
 import { Flavor } from './entities/flavor.entity';
+import { Event } from '../events/entities/event.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Coffee, Flavor])], // Use typeorm mapping entity and sql table
+  imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event])], // Use typeorm mapping entity and sql table
   controllers: [CoffeesController],
   providers: [CoffeesService],
 })
