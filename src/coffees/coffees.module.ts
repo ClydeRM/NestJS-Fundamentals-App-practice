4521d765ce9c1163a22822430d0bf38f4033281d
ClydeRM <clyde.official@icloud.com>
2022-02-06 18:25:37 +0800

    feat(Module): Example for useFactory method
    useFactory() can build custom provider more reusable
    caution 1. Must add Factory class in providers array
    caution 2. useFactory() must call the Factory class method to retrieve data
    caution 3. inject: property must appoint to Factory class

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Injectable, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CoffeesController } from './coffees.controller';
 import { CoffeesService } from './coffees.service';
@@ -7,27 +7,29 @@ import { Coffee } from './entities/coffee.entity';
 import { Flavor } from './entities/flavor.entity';
 import { Event } from '../events/entities/event.entity';
 
+@Injectable()
+export class CoffeeBrandsFactory {
+  create() {
+    /* Do something */
+    return ['buddy brew', 'nescafe'];
+  }
+}
+
 // Static Custom provider
 import { COFFEE_BRANDS } from './coffees.constants';
-
-// Dynamic Custom provider, determine a Class that a Token should resolve to.
-class ConfigService {}
-class DevelopmentConfigService {}
-class ProductConfigService {}
 
 @Module({
   imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event])], // Use typeorm mapping entity and sql table
   controllers: [CoffeesController],
   providers: [
     CoffeesService,
-    { provide: COFFEE_BRANDS, useValue: ['buddy brew', 'nescafe'] }, // Static Custom provider, Inject certain data in own service
+    CoffeeBrandsFactory,
     {
-      provide: ConfigService,
-      useValue:
-        process.env.NODE_ENV === 'development'
-          ? DevelopmentConfigService
-          : ProductConfigService,
-    },
+      provide: COFFEE_BRANDS,
+      useFactory: (brandsFactory: CoffeeBrandsFactory) =>
+        brandsFactory.create(),
+      inject: [CoffeeBrandsFactory],
+    }, // Static Custom provider useFactory method, Inject certain data in own service
   ],
   exports: [CoffeesService],
 })
