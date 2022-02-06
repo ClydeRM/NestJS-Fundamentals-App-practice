b12635b464753dd0602cb32a0a37c3c16586f90b
ClydeRM <clyde.official@icloud.com>
2022-02-06 14:55:21 +0800

    feat(ormconfig): setup typeorm migration setting
    cli: migrationDir (migration file output directory)

---
+++ ormconfig.ts
@@ -0,0 +1,15 @@
+// For TypeOrm data migration
+// Migration Command: >> npx typeorm migration:create -n _migration_file_name_
+module.exports = {
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: 'pass123',
+  database: 'postgres',
+  entities: ['dist/**/*.entity.js'],
+  migrations: ['dist/migrations/*.js'],
+  cli: {
+    migrationsDir: 'src/migrations',
+  },
+};
