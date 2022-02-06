8d00a02aefd78f26e2924f802762020480b8cdfc
ClydeRM <clyde.official@icloud.com>
2022-02-06 17:18:06 +0800

    feat(App): Add new coffee-rating module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
+import { CoffeeRatingModule } from './coffee-rating/coffee-rating.module';
 
 @Module({
   imports: [
@@ -18,6 +19,7 @@ import { CoffeesModule } from './coffees/coffees.module';
       autoLoadEntities: true, // If option not found, try reinstall @nestjs/typeorm
       synchronize: true, // shouldn't be used in production - otherwise you can lose production data #自動產生entity的SQL table
     }),
+    CoffeeRatingModule,
   ],
   controllers: [AppController],
   providers: [AppService],
