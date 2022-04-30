ba3301249da7f79f02a57583d6fe932d5b919915
ClydeRM <clyde.official@icloud.com>
2022-04-30 18:16:23 +0800

    feat(Guard): Check permission with reflector
    Reflector class allow us to retrieve metadata from specific context
    ConfigService class make us to get ENV_Value wisely

--- src/common/guards/api-key.guard.ts
+++ src/common/guards/api-key.guard.ts
@@ -1,15 +1,26 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { Reflector } from '@nestjs/core';
 import { Request } from 'express';
 import { Observable } from 'rxjs';
+import { IS_PUBLIC_KEY } from '../decorators/public.decorator';
 
 @Injectable()
 export class ApiKeyGuard implements CanActivate {
+  constructor(
+    private readonly reflector: Reflector,
+    private readonly configService: ConfigService,
+  ) {}
   // CanActivate interface must return boolean True allowed / False Denied
   canActivate(
     context: ExecutionContext, // inherits the ArgumentsHost
   ): boolean | Promise<boolean> | Observable<boolean> {
+    const isPublic = this.reflector.get(IS_PUBLIC_KEY, context.getHandler());
+    if (isPublic) {
+      return true;
+    }
     const request = context.switchToHttp().getRequest<Request>();
     const authHeader = request.header('Authorization'); // 取得標頭"權限"欄位值
-    return authHeader === process.env.API_KEY;
+    return authHeader === this.configService.get('API_KEY');
   }
 }
