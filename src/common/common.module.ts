61240fb728479276dd7f29868f0c4f1ff17f38b6
ClydeRM <clyde.official@icloud.com>
2022-05-01 18:46:39 +0800

    feat(Module): Inject middleware in module
    Implement NestModule interface provide a 'configure()' method
    consumer must apply one middleware
    forRoutes() can pass in a string or object specify route path
    exclud() can exclud specify route path

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,10 +1,29 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { APP_GUARD } from '@nestjs/core';
+// Custom Guard
 import { ApiKeyGuard } from './guards/api-key.guard';
+// Custom middleware
+import { LoggingMiddleware } from './middlewares/logging.middleware';
 
 @Module({
   imports: [ConfigModule],
   providers: [{ provide: APP_GUARD, useClass: ApiKeyGuard }],
 })
-export class CommonModule {}
+export class CommonModule implements NestModule {
+  // Every custom middleware inject in Module must implement NestModule interface
+  // Every custom middleware must provide a 'configure()' method
+  configure(consumer: MiddlewareConsumer) {
+    // consumer provides a set of useful methods to tie middleware to "specific routes"
+    consumer.apply(LoggingMiddleware).forRoutes('*'); // * represent all route
+    // consumer.apply(LoggingMiddleware).exclude('coffees').forRoutes('*'); // exclude all /coffees routes
+    // consumer
+    //   .apply(LoggingMiddleware)
+    //   .forRoutes({ path: 'coffees', method: RequestMethod.GET }); // specify GET /coffees routes
+  }
+}
