9ea80fa822568f8ab4c2fb0136f2abdb7dd4eef8
ClydeRM <clyde.official@icloud.com>
2022-05-01 19:04:48 +0800

    feat(Decorator): Custom Decorator
    Implement createParamDecorator interface
    use ExecutionContext to get http context

---
+++ src/common/decorators/protocol.decorator.ts
@@ -0,0 +1,11 @@
+// Custom Decorator example
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+
+export const Protocol = createParamDecorator(
+  (defaultValue: string, ctx: createParamDecorator) => {
+    console.log({ defaultValue });
+
+    const request = ctx.switchToHttp().getRequest();
+    return request.protocol;
+  },
+);
