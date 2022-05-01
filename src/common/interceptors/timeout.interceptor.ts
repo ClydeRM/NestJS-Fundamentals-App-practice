27e868713184d2d84c2e81e01768118742cb8c18
ClydeRM <clyde.official@icloud.com>
2022-05-01 17:54:11 +0800

    feat(Interceptor): Custom an Interceptor
    Timeout interceptor helps us handle all responses
    has executed lots of time

---
+++ src/common/interceptors/timeout.interceptor.ts
@@ -0,0 +1,25 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+  RequestTimeoutException,
+} from '@nestjs/common';
+import { Observable, throwError, TimeoutError } from 'rxjs';
+import { catchError, timeout } from 'rxjs/operators';
+
+@Injectable()
+export class TimeoutInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    // If a req or res execute more than 3 sec. ,Interceptor is going to end handler forcefully
+    return next.handle().pipe(
+      timeout(3000),
+      catchError((err) => {
+        if (err instanceof TimeoutError) {
+          return throwError(new RequestTimeoutException());
+        }
+        return throwError(err);
+      }),
+    );
+  }
+}
