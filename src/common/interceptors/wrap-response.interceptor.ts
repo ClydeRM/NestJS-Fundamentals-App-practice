4bba4728fca1fab99796ff50b86fcc2bfb7af412
ClydeRM <clyde.official@icloud.com>
2022-05-01 16:44:19 +0800

    feat(Interceptor): Custom an Interceptor
    Test interceptor lifetime between the start and end
    of the route handler (response)

---
+++ src/common/interceptors/wrap-response.interceptor.ts
@@ -0,0 +1,28 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
+
+// Wrap Req and Res stream, allow us to decide execute the route or not
+@Injectable()
+export class WrapResponseInterceptor implements NestInterceptor {
+  // Every CustomInterceptor must implement NestInterceptor interface
+  // Every CustomInterceptor must have one intercept method
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    // Every intercept method must return a 'Observable' from Rxjs library
+    // CallHandler interface implement handle method. Call it can invoke the route handler method, If don't not invoke
+    console.log('Before...'); // Execute it before the route handler be called
+
+    // return next.handle().pipe(tap((data) => console.log('After...', data))); // Execute it after the route handler be called
+    // tap() invokes an anonymous logging function upon graceful termination of the Observable stream
+    // data => ... is the response we send back to the router handler
+
+    return next.handle().pipe(map((data) => ({ data })));
+    // map() takes a value from the stream and returns a modified one
+    // wrap response data in a object
+  }
+}
