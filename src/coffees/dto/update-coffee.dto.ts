9fb17ff3c362e422a9a738577cbe4e9324e9491f
ClydeRM <clyde.official@icloud.com>
2022-05-02 17:02:45 +0800

    chore(Dto): Change Lib ensure swagger get schema

--- src/coffees/dto/update-coffee.dto.ts
+++ src/coffees/dto/update-coffee.dto.ts
@@ -1,7 +1,8 @@
 // CLI >> nest g class coffees/dto/update-coffee.dto --no-spec
 // Update property optional
 
-import { PartialType } from '@nestjs/mapped-types'; // 利用原本的DTO去建立一個內容元素是可選擇使用的新DTO
+// import { PartialType } from '@nestjs/mapped-types'; // 利用原本的DTO去建立一個內容元素是可選擇使用的新DTO
+import { PartialType } from '@nestjs/swagger'; // Insure Swagger UI can get all partial type properties
 import { CreateCoffeeDto } from './create-coffee.dto'; // 原本的DTO
 
 export class UpdateCoffeeDto extends PartialType(CreateCoffeeDto) {}
