0f6de7fe72855bdca038d021ccb8a0769d6adb43
ClydeRM <clyde.official@icloud.com>
2022-05-05 17:19:10 +0800

    feat(App e2e test): A e2e test sample
    afterAll(): close all promise promise avoid jest warning

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -6,19 +6,26 @@ import { AppModule } from './../src/app.module';
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
+    // Change beforeEach to beforeAll, bc we don't want to restart the app for each e2e test
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
-    app = moduleFixture.createNestApplication();
+    app = moduleFixture.createNestApplication(); // 一定要建立一個App runtime system
     await app.init();
   });
 
+  // it('desc', func(supertest.request(){...}))
   it('/ (GET)', () => {
-    return request(app.getHttpServer())
+    return request(app.getHttpServer()) // Connect to server (Express or Fastify)
       .get('/')
+      .set('Authorization', process.env.API_KEY) // Pass to Global Guard
       .expect(200)
       .expect('Hello World!');
+  });
+
+  afterAll(async () => {
+    await app.close(); // 因為App有一些“非同步”的程序，這邊是DB connecter
   });
 });
