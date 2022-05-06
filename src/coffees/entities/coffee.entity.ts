ba7bbfdcb35d57030ab6808bb9118ece85471b4a
ClydeRM <clyde.official@icloud.com>
2022-05-06 12:45:13 +0800

    feat(Entity): Create Coffee entity

--- src/coffees/entities/coffee.entity.ts
+++ src/coffees/entities/coffee.entity.ts
@@ -1,7 +1,21 @@
 // Definition of coffee data entity.
-export class Coffee {
-  id: number;
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+@Schema()
+export class Coffee extends Document {
+  @Prop()
   name: string;
+
+  @Prop()
   brand: string;
+
+  @Prop({ default: 0 })
+  recommendation: number;
+
+  @Prop([String])
   flavors: string[];
 }
+
+export const CoffeesSchema = SchemaFactory.createForClass(Coffee);
+// DefinitionsFactoryClass 建立一列（raws）的 Schema instances
